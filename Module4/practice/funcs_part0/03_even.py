b75793b21dde1962572f2ac0619919bde4b13e16
VladimirKorolev1986 <106909929+VladimirKorolev1986@users.noreply.github.com>
2022-06-19 15:23:43 +0300

    Update 03_even.py

--- Module4/practice/funcs_part0/03_even.py
+++ Module4/practice/funcs_part0/03_even.py
@@ -1,9 +1,11 @@
-def even(...):
-    ...
-    return ...
+def even(a):
+    if a % 2 == 0:
+        return 1
+    return not (1)
 
-n = ...
+
+n =0
 if even(n):
-   print("Число четное")
+    print("Число четное")
 else:
-   print("Число не четное")
+    print("Число не четное")
