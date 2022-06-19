fa574cf2045c98d5d9f041c0abc2b4dc1546a438
VladimirKorolev1986 <106909929+VladimirKorolev1986@users.noreply.github.com>
2022-06-19 11:55:49 +0300

    Update 02_task_list.py

--- Module3/practice/list_and_tuple/02_task_list.py
+++ Module3/practice/list_and_tuple/02_task_list.py
@@ -3,4 +3,9 @@
 
 fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
 
-# TODO: your code here
+fruits = ["яблоко", "банан", "киви", "ананас", "груша"]
+n = 1
+for fruit in fruits:
+    print(n, " ", fruit)
+    n += 1
+
